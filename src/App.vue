9e39d0a82f2721192864f9557effc9ca3ca16581
NTA Le Tien Dat <datlt.nta@gmail.com>
2022-08-21 23:09:43 +0700

    form data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <FormInput />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FormInput from "./components/FormInput.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FormInput,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
