8dad1c06541ba9d8bf3ffbd08a5f9abe955c390b
NTA LE TIEN DAT <datlt.nta@gmail.com>
2022-08-23 08:58:30 +0700

    learn composition vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,50 @@
 <template>
   <h1>Overview is here</h1>
+  <input type="text" v-model="keyword" />
+  <ul>
+    <li v-for="searchFilter in searchFilters" :key="searchFilter">
+      {{ searchFilter }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import { ref, reactive, computed, watch, watchEffect } from "vue";
 export default {
   name: "HomePage",
+  setup() {
+    const keyword = ref("");
+    const arraySearchs = reactive([
+      "hi",
+      "hifo",
+      "hifoxo",
+      "fo",
+      "fofi",
+      "fofiso",
+    ]);
+
+    const searchFilters = computed(() =>
+      arraySearchs
+        .map((arraySearch) => {
+          return arraySearch.toLowerCase();
+        })
+        .filter((arraySearch) =>
+          arraySearch.includes(keyword.value.toLowerCase())
+        )
+    );
+
+    watch(keyword, (oldValue, newValue) => {
+      console.log(oldValue, newValue);
+    });
+
+    watchEffect(() => {
+      if (keyword.value) {
+        console.log("running");
+      }
+    });
+
+    return { keyword, arraySearchs, searchFilters };
+  },
 };
 </script>
 
